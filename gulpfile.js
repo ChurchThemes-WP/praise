48596a7ca337a782a8d1adfe580dd95e1b870c8a
Anthony Garand <garand@garand.me>
2015-06-12 12:08:38 -0400

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var minifyCSS    = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  gulp.src('./assets/style/**/*.scss')
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./assets/style/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch'], function() {});
+gulp.task('build', ['sass'], function() {});
