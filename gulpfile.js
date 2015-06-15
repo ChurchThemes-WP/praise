5058e6df1ccc56b66809f2543f3bd322f13babd4
Anthony Garand <garand@garand.me>
2015-06-15 14:36:22 -0400

    Add updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,47 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
+var notify       = require("gulp-notify");
+var browserSync  = require('browser-sync');
 var minifyCSS    = require('gulp-minify-css');
+var connect      = require('gulp-connect-php');
 var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('connect', function() {
+  connect.server({
+    base: '.wordpress/web',
+    hostname: '127.0.0.1',
+    port: 8000,
+    open: false
+  }, function () {
+    browserSync({
+      proxy: '127.0.0.1:8000',
+      port: 8080,
+      notify: false,
+      ghostMode: false
+    });
+  });
+});
 
 gulp.task('sass', function () {
   gulp.src('./assets/style/**/*.scss')
     .pipe(sass({
-      errLogToConsole: true
+      errLogToConsole: false
     }))
+    .on('error', notify.onError())
     .pipe(autoprefixer())
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(notify({ message: 'Build Complete' }));
+});
+
+gulp.task('reload', function () {
+  browserSync.reload();
 });
 
 gulp.task('watch', function() {
   gulp.watch('./assets/style/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['build','watch'], function() {});
+gulp.task('default', ['build', 'connect','watch'], function() {});
 gulp.task('build', ['sass'], function() {});
